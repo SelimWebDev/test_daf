8fc4d12b584faf0948d883e6e0282755f1bbac83
Selim El Mahi <selim.elmahi@gmail.com>
2023-06-13 13:47:15 +0200

    'finalisation'

--- controllers/subscriptionCtrl.js
+++ controllers/subscriptionCtrl.js
@@ -1,6 +1,7 @@
 const Subscription = require('../models/subscription')
 
 exports.subscriptionContractList = async (req, res, next) => {
+    
 
     let options = {
         select: 'start_date',
@@ -8,11 +9,16 @@ exports.subscriptionContractList = async (req, res, next) => {
         where: [{
             'current_status' : 'processing',
             'SubscriptionType.name' : 'MAJ',
-            'start_date' : '{ &gte: 30/12/2018}'
+            'start_date' : { $gte: new Date("12-30-2018") }
         }]
     }
 
-    const subscriptions = await Subscription.find(options.where[0], options.select).limit(options.limit)
-    res.send(subscriptions) 
+    try {
+        const subscriptions = await Subscription.find(options.where[0], options.select).limit(options.limit)
+        res.send(subscriptions) 
+    } catch (error){
+        res.send(error)
+        console.log(error)
+    }
 };
 
